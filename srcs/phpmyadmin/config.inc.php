cbf36dae7a1459dd81ac3012d75d004462f940ae
Ubuntu <ubuntu@ip-172-31-43-217.us-west-1.compute.internal>
2020-12-18 05:38:19 +0000

    wordpress not loading, phpymyadmin cannot connect to mysql

---
+++ srcs/phpmyadmin/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+$cfg['blowfish_secret'] = 'GThjIY]T0U5[dJ,4LfVhhJbPiidmsGO8';
+
+$i = 1;
+
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+$cfg['Servers'][$i]['host'] = "mysql-service";
+$cfg['Servers'][$i]['port'] = "3306";
+$cfg['Servers'][$i]['user'] = "dbadmin";
+$cfg['Servers'][$i]['password'] = "B42bXZhJuO1w4ibhXCQm";
+
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -38,8 +38,8 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-#define('WP_HOME', 'http://192.168.49.2:5050');
-#define('WP_SITEURL', 'http://192.168.49.2:5050');
+define('WP_HOME', 'http://192.168.49.2:5050');
+define('WP_SITEURL', 'http://192.168.49.2:5050');
 
 /**#@+
  * Authentication Unique Keys and Salts.
