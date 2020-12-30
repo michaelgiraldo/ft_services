4f4380ebe65b2d8d8c12afd95d5418d6e0eb5159
Ubuntu <ubuntu@ip-172-31-43-217.us-west-1.compute.internal>
2020-12-30 06:16:18 +0000

    fixed wordpress and nginx 22

--- srcs/nginx/entrypoint.sh
+++ srcs/nginx/entrypoint.sh
@@ -4,6 +4,25 @@ nginx -t
 nginx
 #php-fpm7
 #/etc/init.d/sshd start
-sshd &
+
+# https://github.com/danielguerra69/alpine-sshd/blob/master/docker-entrypoint.sh
+
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+	# generate fresh rsa key
+	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+fi
+if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
+	# generate fresh dsa key
+	ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+fi
+
+#prepare run dir
+if [ ! -d "/var/run/sshd" ]; then
+  mkdir -p /var/run/sshd
+fi
+
+/usr/sbin/sshd -D
 telegraf &
 tail -f /dev/null
+
+exec "$@"
